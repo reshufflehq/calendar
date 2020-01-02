fc9c9266fd2a3a1bbb75b8c34d9cfbba15aed9b9
Havi Rotem <havi@reshuffle.com>
2020-01-02 17:29:21 +0200

    Add tests Update react-form Update config.yml
    Update config.yml - add new workflow

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Children } from 'react';
 import App from './App';
-import { NewEventDialog, mutationAddEvent } from './components/NewEventDialog';
-import { Calendar, eventsList } from './Calendar';
+import { NewEventDialog, mutationAddEvent } from './components/NewEventDialog'
+import { Calendar, eventsList} from './Calendar'
 import { MockedProvider, wait } from '@apollo/react-testing';
 import { create, act } from 'react-test-renderer';
 
-it('renders without crashing', async () => {
+it('renders without crashing', async() => {
   create(
     <MockedProvider>
       <App />
@@ -21,28 +21,64 @@ const Initialmocks = [
       variables: {},
     },
     result: {
-      getEvents: [
-        {
-          key: 'events/YIv1_MrY_0EibooqGWQSO',
-          data: {
-            id: 'YIv1_MrY_0EibooqGWQSO',
-            title: 'Next Week Planning',
-            start: '2020-01-15T10:30:00.000Z',
-            end: '2020-01-15T12:30:00.000Z',
-            description: '',
-          },
+      getEvents: [{
+        "key": "events/YIv1_MrY_0EibooqGWQSO",
+        "data": {
+            "id": "YIv1_MrY_0EibooqGWQSO",
+            "title": "Next Week Planning",
+            "start": "2020-01-15T10:30:00.000Z",
+            "end": "2020-01-15T12:30:00.000Z",
+            "description": ""
         },
-      ],
+      }],
     },
   },
 ];
 
-it('renders Calendar with initial event', async () => {
+it('renders Calendar with initial event', async() => {
   create(
     <MockedProvider mocks={Initialmocks} addTypename={false}>
       <App>
-        <Calendar />
+        <Calendar/>
+      </App>
+    </MockedProvider>,
+  );
+});
+
+const addEventMock = [
+  {
+    request: {
+      query: mutationAddEvent,
+      variables: {},
+    },
+    result: {
+      addEvent: {
+            "id": "random_id",
+            "title": "New Event",
+            "start": "",
+            "end": "2020-01-15T12:30:00.000Z",
+            "description": "yada yada yada"
+      },
+    },
+  },
+];
+
+it('add event failes when adding event without start time', async() => {
+  const component = create(
+    <MockedProvider mocks={addEventMock} addTypename={false}>
+      <App>
+        <Calendar>
+          <NewEventDialog
+            open={true}
+            onClose={Function}
+            selectedEvent={''}
+            />
+        </Calendar>
       </App>
     </MockedProvider>
   );
+  await act(() => wait());
+
+  const testInstance = component.root;
+  expect(testInstance.findByType('p').children).toEqual(['ERROR']);
 });
